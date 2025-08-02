14339f0ed79339968657ee796929e5ae09ff2ecb
yashpatel-py <yashpatelpvt.21@gmail.com>
2025-08-02 00:37:50 -0400

    first commit

---
+++ 001_first_program/hello_world.py
@@ -0,0 +1,28 @@
+# This is my first program
+
+'''
+This 
+is 
+my 
+first
+program
+'''
+
+"""
+This 
+is 
+my 
+first
+program
+"""
+
+print('Hello, world')
+
+print('''
+This 
+is 
+my 
+first
+program
+''')
+
