f4c236e0d1ec01a25e79441cb30b8e5e223d49a0
yashpatel-py <yashpatelpvt.21@gmail.com>
2025-08-07 23:22:06 -0400

    User Inputs

---
+++ 003_python_inputs/1_input_function.py
@@ -0,0 +1,7 @@
+# name = input("Please enter your name: ")
+# print("My name is", name)
+
+age = input("Please enter your age: ")
+age_number = float(age)
+age_next_year = age_number+1
+print(age_next_year)
